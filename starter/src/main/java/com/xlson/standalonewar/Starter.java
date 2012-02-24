801a11637ac6a361f1a5dea681d5d0315e207a07
Konstantin Boudnik <cos@karmasphere.com>
2012-02-24 14:53:32 -0800

    Updating Starter to work with Jetty7

--- starter/src/main/java/com/xlson/standalonewar/Starter.java
+++ starter/src/main/java/com/xlson/standalonewar/Starter.java
@@ -16,10 +16,10 @@
 
 package com.xlson.standalonewar;
 
-import org.mortbay.jetty.Connector;
-import org.mortbay.jetty.Server;
-import org.mortbay.jetty.bio.SocketConnector;
-import org.mortbay.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.bio.SocketConnector;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 import java.net.URL;
 import java.security.ProtectionDomain;
@@ -30,45 +30,46 @@ import java.security.ProtectionDomain;
  * start a Jetty instance on port 8080 with the containing war loaded.
  * If you specify a port environment variable that will be used for Jetty
  * instead of 8080 (see example).
- *
+ * <p/>
  * The base of this class comes from this blogpost:
  * http://eclipsesource.com/blogs/2009/10/02/executable-wars-with-jetty/
- *
+ * <p/>
  * Example:
  * java -jar -Dport=80 webapp.war
- *
+ * <p/>
  * webapp.war is loaded on http://localhost:8080/
  *
  * @author Leonard Axelsson
  */
 public class Starter {
-	
-	private static final int DEFAULT_PORT = 8080;
 
-    public static void main(String[] args) throws Exception {
-        Server server = new Server();
-        SocketConnector connector = new SocketConnector();
-        connector.setMaxIdleTime(1000 * 60 * 60);
-        connector.setSoLingerTime(-1);
-        connector.setPort(Integer.getInteger("port", DEFAULT_PORT));
-        server.setConnectors(new Connector[]{connector});
+  private static final int DEFAULT_PORT = 8080;
 
-        WebAppContext context = new WebAppContext();
-        context.setServer(server);
-        context.setContextPath("/");
+  public static void main(String[] args) throws Exception {
+    Server server = new Server();
+    SocketConnector connector = new SocketConnector();
+    connector.setMaxIdleTime(1000 * 60 * 60);
+    connector.setSoLingerTime(-1);
+    connector.setPort(Integer.getInteger("port", DEFAULT_PORT));
+    server.setConnectors(new Connector[]{connector});
 
-        ProtectionDomain protectionDomain = Starter.class.getProtectionDomain();
-        URL location = protectionDomain.getCodeSource().getLocation();
-        context.setWar(location.toExternalForm());
+    WebAppContext context = new WebAppContext();
+    context.setServer(server);
+    context.setContextPath("/");
 
-        server.addHandler(context);
-        try {
-            server.start();
-            server.join();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(100);
-        }
+    ProtectionDomain protectionDomain = Starter.class.getProtectionDomain();
+    URL location = protectionDomain.getCodeSource().getLocation();
+    context.setWar(location.toExternalForm());
 
+    server.setHandler(context);
+    try {
+      server.start();
+      System.in.read();
+      server.join();
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.exit(100);
     }
+
+  }
 }
