8df2b4a0ef7cd9709c2e3736c253a6b5a022408c
Antony Riley <antony@karmasphere.com>
2012-10-12 15:46:58 -0700

    Make more things configurable both from CLI and webapp jar contents.

--- starter/src/main/java/com/xlson/standalonewar/Starter.java
+++ starter/src/main/java/com/xlson/standalonewar/Starter.java
@@ -21,8 +21,11 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.bio.SocketConnector;
 import org.eclipse.jetty.webapp.WebAppContext;
 
+import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
 import java.security.ProtectionDomain;
+import java.util.Properties;
 
 /**
  * Starter for embedded Jetty. This class is intended to be packaged into a
@@ -43,19 +46,55 @@ import java.security.ProtectionDomain;
  */
 public class Starter {
 
-  private static final int DEFAULT_PORT = 8080;
+  public static final String CONFIG = "/WEB-INF/standalonify.properties";
+
+  public static final String PROP_PORT = "app.port";
+  public static final String PROP_CONTEXT = "app.context";
+  public static final String PROP_TMP = "app.tmp";
+
+  public static final String DEFAULT_PORT = "8080";
+  public static final String DEFAULT_CONTEXT = "/";
+
+  private static final Properties props = new Properties();
+
+  public static String getProperty(String property, String dflt) {
+    String ret = System.getProperty(property);
+    if (ret != null)
+      return ret;
+    ret = props.getProperty(property);
+    if (ret != null)
+      return ret;
+    return dflt;
+  }
 
   public static void main(String[] args) throws Exception {
+    // Load properties.
+    InputStream in = Starter.class.getResourceAsStream(CONFIG);
+    if (in != null) {
+        props.load(in);
+        in.close();
+    }
+
+    String port = getProperty(PROP_PORT, DEFAULT_PORT);
+    String contextPath = getProperty(PROP_CONTEXT, DEFAULT_CONTEXT);
+    String tmp = getProperty(PROP_TMP, null);
+
     Server server = new Server();
     SocketConnector connector = new SocketConnector();
     connector.setMaxIdleTime(1000 * 60 * 60);
     connector.setSoLingerTime(-1);
-    connector.setPort(Integer.getInteger("port", DEFAULT_PORT));
+    connector.setPort(Integer.parseInt(port));
     server.setConnectors(new Connector[]{connector});
 
     WebAppContext context = new WebAppContext();
     context.setServer(server);
-    context.setContextPath(System.getProperty("app.context", "/"));
+    context.setContextPath(contextPath);
+    if (tmp != null) {
+      File tmpDir = new File(tmp);
+      if (!tmpDir.exists())
+        tmpDir.mkdirs();
+      context.setTempDirectory(tmpDir);
+    }
 
     ProtectionDomain protectionDomain = Starter.class.getProtectionDomain();
     URL location = protectionDomain.getCodeSource().getLocation();
@@ -70,6 +109,5 @@ public class Starter {
       e.printStackTrace();
       System.exit(100);
     }
-
   }
 }
